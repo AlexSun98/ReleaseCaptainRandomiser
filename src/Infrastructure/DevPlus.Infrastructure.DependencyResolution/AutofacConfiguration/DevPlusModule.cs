8b308e1a692c5f730fc00dc6df73032c2331f92a
Alex Sun <alexsun8866@gmail.com>
2017-07-05 20:39:27 +1000

    folder change

---
+++ src/Infrastructure/DevPlus.Infrastructure.DependencyResolution/AutofacConfiguration/DevPlusModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+//using DevPlus.Repositories.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevPlus.Infrastructure.DependencyResolution.AutofacConfiguration
+{
+    public class DevPlusModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
+
+            //builder.RegisterType<DatabaseInitializer>().As<IDatabaseInitializer>().InstancePerDependency();
+        }
+    }
+}
---
+++ src/Infrastructure/DevPlus.Infrastructure.DependencyResolution/Dependencybootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Autofac.Core;
+//using DevPlus.Domain;
+using DevPlus.Infrastructure.DependencyResolution.AutofacConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DevPlus.Infrastructure.DependencyResolution
+{
+    public class DependencyBootstrapper
+    {
+
+        internal void RegisterAllDependenciesOnStartup()
+        {      
+            ConfigureAutofac();
+        }
+        private void ConfigureAutofac()
+        {
+            // Perform registrations and build the container.
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new DevPlusModule());
+            var container = builder.Build();
+
+            //var csl = new AutofacServiceLocator(container);
+            //ServiceLocator.SetLocatorProvider(() => csl);
+
+            //CoreServiceLocator.SetServiceLocator(() => new AutofacServiceLocator(container));
+        }
+
+    }
+}
