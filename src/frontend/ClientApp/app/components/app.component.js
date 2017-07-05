8e523f50b07dec9099882fd09f6043ad4f3adcc1
Alex Sun <alexsun8866@gmail.com>
2017-07-05 20:37:19 +1000

    removed old frontend folders

--- src/frontend/ClientApp/app/components/app.component.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var AppComponent = (function () {
-    function AppComponent() {
-    }
-    return AppComponent;
-}());
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
